f4933811ead62db39998f641ebc107bab176398f
Juliana Daroz <julianadaroz@gmail.com>
2022-11-08 11:24:01 -0300

    Added eslint config by rocketseat

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { Button } from "./components/Button";
-import { ThemeProvider } from "styled-components";
+import { Button } from './components/Button'
+import { ThemeProvider } from 'styled-components'
 
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
-    return (
-        <ThemeProvider theme={defaultTheme}>
-            <Button variant="primary" />
-            <Button variant="secondary" />
-            <Button variant="sucess" />
-            <Button variant="danger" />
-            <Button />
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="sucess" />
+      <Button variant="danger" />
+      <Button />
 
-            <GlobalStyle />
-        </ThemeProvider>
-    );
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,26 +1,19 @@
-import styled, { css } from "styled-components";
+import styled from 'styled-components'
 
-export type ButtonVariant = "primary" | "secondary" | "danger" | "sucess";
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'sucess'
 
 interface ButtonContainerProps {
-    variant: ButtonVariant;
+  variant: ButtonVariant
 }
 
-const buttonVariants = {
-    primary: "purple",
-    secondary: "orange",
-    danger: "red",
-    sucess: "green",
-};
-
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-    width: 100px;
-    height: 40px;
+  width: 100px;
+  height: 40px;
 
-    border-radius: 4px;
-    border: 0;
-    margin: 8px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
-    background-color: ${(props) => props.theme["green-500"]};
-    color: ${(props) => props.theme.white};
-`;
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
+`
